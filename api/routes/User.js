e1735d097374f8d62ad6c98b50e710fd48f1a1f0
Ian Cossa <ianrafaelelias@gmail.com>
2025-03-18 11:06:25 -0700

    Login Part

--- api/routes/User.js
+++ api/routes/User.js
@@ -98,6 +98,57 @@ router.post("/singup", (req, res) => {
   }
 });
 
-//Sign In
-router.post("/signin", (req, res) => {});
+//Sign In Or Login
+router.post("/signin", (req, res) => {
+  let { email, password } = req.body;
+  email = email.trim();
+  password = password.trim();
+
+  if (email == "" || password == "") {
+    res.json({
+      status: "FAILED",
+      message: "Empty credentials supplied",
+    });
+  } else {
+    //check if user exist
+    User.find({ email })
+      .then((data) => {
+        if (data) {
+          //User exists
+
+          const hashedPassword = data[0].password;
+          bcrypt
+            .compare(password, hashedPassword)
+            .then((result) => {
+              if (result) {
+                res.json({
+                  status: "SUCCESS",
+                  message: "Signin successful",
+                  data: data,
+                });
+              } else {
+                res.json({
+                  status: "FAILED",
+                  message: "Invalid password entered!",
+                });
+              }
+            })
+            .catch((err) => {
+              status: "An error occured while comparing passwords";
+            });
+        } else {
+          res.json({
+            status: "FAILED",
+            message: "Invalid credentials entered!",
+          });
+        }
+      })
+      .catch((err) => {
+        res.json({
+          status: "FAILED",
+          message: "An error occured while checking for existing  user",
+        });
+      });
+  }
+});
 module.exports = router;
